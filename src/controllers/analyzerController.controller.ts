eba904ddefd3d130f54de444a94df829a550f2fb
Michal Kus <michal513717@gmail.com>
2025-06-04 16:43:45 +0200

    Conected validator for analyze message request

--- src/controllers/analyzerController.controller.ts
+++ src/controllers/analyzerController.controller.ts
@@ -1,5 +1,9 @@
 import type { Request, Response } from "express";
 import { MessageAnalyzer } from "../managers/messageAnalzyer";
+import { analyzeMessageValidator } from "../validators/analyzeMessege.validator";
+import { internalServerErrorResponse, validationErrorResponse } from "../utils/responses";
+import { validationResult } from "express-validator";
+import { ErrorWithCode, ValidationError } from "../utils/errors";
 
 export class AnalyzerController {
     constructor(
@@ -7,14 +11,32 @@ export class AnalyzerController {
     ) { }
 
     async analyzeMessage(req: Request, res: Response): Promise<any> {
-        const result = await this.messageAnalyzer.exec(req.body.message);
+        try {
+            await Promise.all(analyzeMessageValidator.map(validation => validation.run(req)));
 
-        res.status(200).send({
-            result: {
-                keywords: result.keywords,
-                urgencyIndicators: result.urgencyIndicators,
-                priorityScore: result.priorityScore
+            if (validationResult(req).isEmpty() === false) {
+                throw new ValidationError();
             }
-        })
+
+            const result = await this.messageAnalyzer.exec(req.body.message);
+
+            res.status(200).send({
+                result: {
+                    keywords: result.keywords,
+                    urgencyIndicators: result.urgencyIndicators,
+                    priorityScore: result.priorityScore
+                }
+            })
+        } catch (error) {
+            if (validationResult(req).isEmpty() === false) {
+                return validationErrorResponse(res, validationResult(req).array());
+            }
+
+            if (error instanceof ErrorWithCode) {
+                return res.status(error.status).json(error.toJSON());
+            }
+
+            return internalServerErrorResponse(res);
+        }
     }
 }
